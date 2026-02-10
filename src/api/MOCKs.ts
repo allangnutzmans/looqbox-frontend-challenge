ac8c5329e8853051158f4f0951399e88b1a1fb2c
Allan Gnutzmans <allan.sgnutzman@gmail.com>
2026-02-09 23:35:22 -0300

    feat(api): add pokemon models and detailed mock data

--- src/api/MOCKs.ts
+++ src/api/MOCKs.ts
@@ -89,7 +89,14 @@ export const POKEDEX_LIST_BASE = {
     ]
 }
 
-export const POKEDEX_LIST_DETAILS = [
+export type Pokemon = {
+    id: number;
+    name: string;
+    image: string;
+    types: string[];
+}
+
+export const POKEDEX_LIST_DETAILS: Pokemon[] = [
     {
         "id": 1,
         "name": "bulbasaur",
@@ -262,3 +269,25 @@ export const POKEDEX_LIST_DETAILS = [
         ]
     }
 ]
+
+// Probably will remain as a constant
+export const POKE_TYPES: Record<string, string> = {
+  normal: "#A8A77A",
+  fire: "#EE8130",
+  water: "#6390F0",
+  electric: "#F7D02C",
+  grass: "#21FF00",
+  ice: "#96D9D6",
+  fighting: "#C22E28",
+  poison: "#A33EA1",
+  ground: "#E2BF65",
+  flying: "#A98FF3",
+  psychic: "#F95587",
+  bug: "#A6B91A",
+  rock: "#B6A136",
+  ghost: "#735797",
+  dragon: "#6F35FC",
+  dark: "#705746",
+  steel: "#B7B7CE",
+  fairy: "#D685AD"
+};
