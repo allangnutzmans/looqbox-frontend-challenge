79fbb0ba933fc5c101fca41383d04665c636c9d8
Allan Gnutzmans <allan.sgnutzman@gmail.com>
2026-02-11 20:22:23 -0300

    feat(api): implement PokeAPI client with RTK Query

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,7 +1,37 @@
-import { POKEDEX_LIST_DETAILS } from "./MOCKs"
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
+import type { Pokemon } from "./POKE_DETAILS";
 
-export const client = {
-    pokedex: {
-        list: () => POKEDEX_LIST_DETAILS
-    }
+// Relevant types
+interface PokemonListResult {
+    name: string;
+    url: string;
 }
+
+interface ListResponse<T> {
+    count: number
+    next: string | null
+    previous: string | null
+    results: T[]
+}
+
+const BASE_URL = "https://pokeapi.co/api/v2";
+
+//type this
+export const client = createApi({
+    baseQuery: fetchBaseQuery({
+        baseUrl: BASE_URL
+    }),
+    endpoints: (builder) => ({
+        getPokedexList: builder.query<ListResponse<PokemonListResult>, void>({
+            query: () => `pokemon?limit=21&offset=0`
+        }),
+        getPokemonById: builder.query<Pokemon, string>({
+            query: (id) => `pokemon/${id}`
+        })
+    })
+})
+
+
+export const { useGetPokedexListQuery, usePrefetch, useGetPokemonByIdQuery } = client
+
+    
