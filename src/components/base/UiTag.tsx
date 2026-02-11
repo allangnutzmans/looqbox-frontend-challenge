522d0d5486920ec7059448977ecb20cdba6b39aa
Allan Gnutzmans <allan.sgnutzman@gmail.com>
2026-02-11 00:21:45 -0300

    feat(ui): implement UiTag component and unit conversion helpers

---
+++ src/components/base/UiTag.tsx
@@ -0,0 +1,28 @@
+import { Tag, type TagProps } from "antd"
+
+const UiTag = ({
+    name,
+    color,
+    props
+}: {
+    name: string,
+    color?: string,
+    props?: TagProps
+}) => {
+    return (
+        <Tag
+            variant="solid"
+            style={{
+                fontWeight: 'bold',
+                fontSize: '1em',
+                textTransform: 'uppercase',
+            }}
+            color={color}
+            {...props}
+        >
+            {name}
+        </Tag>
+    )
+}
+
+export default UiTag;
---
+++ src/utils/helpers.ts
@@ -0,0 +1,9 @@
+const MAX_STAT = 255; 
+// Some pokemons have stats higher than 255, so normalize with percentage
+export const calcPokeStatsPercentage = (stat: number) => Math.round(stat / MAX_STAT * 100);
+
+//Gets height in meters
+export const getPokeHeight = (height: number) => height / 10;
+
+//Gets weight in kilograms
+export const getPokeWeight = (weight: number) => weight / 10;
